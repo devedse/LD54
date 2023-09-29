ca7a76aff3f82d258464563e8262dafb4f43238b
Devedse <davydavidse@gmail.com>
2023-09-30 01:19:43 +0200

    Unity server is now done

--- UnityGameServer/UnityGameServer/Hubs/UltraHub.cs
+++ UnityGameServer/UnityGameServer/Hubs/UltraHub.cs
@@ -1,13 +1,47 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
 
 namespace UnityGameServer.Hubs
 {
     public class UltraHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        // Use ConcurrentBag to ensure thread safety when adding or checking room names
+        private static readonly ConcurrentBag<string> roomNames = new ConcurrentBag<string>();
+
+        public string CreateUniqueRoomName()
         {
-            Console.WriteLine($"Received a message from: {user} with the message: {message}");
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            string roomName;
+            do
+            {
+                roomName = CreateRandomRoomName();
+            } while (roomNames.Contains(roomName));
+
+            roomNames.Add(roomName);
+            return roomName;
+        }
+
+        private string CreateRandomRoomName()
+        {
+            var random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return new string(Enumerable.Repeat(chars, 4)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public Task Server_CreateRoom()
+        {
+            string roomName = CreateUniqueRoomName();
+            return Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+        }
+
+        public Task Client_JoinRoom(string roomName)
+        {
+            return Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+        }
+
+        public Task Client_SendButtonPress(int button)
+        {
+            return Clients.Others.SendAsync("Server_ReceiveButtonPress", button);
         }
     }
 }
