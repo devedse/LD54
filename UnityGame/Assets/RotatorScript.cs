481c132fce506142ec2edbe80bd89e98add72ef9
Devedse <davydavidse@gmail.com>
2023-09-29 22:00:54 +0200

    Update rotato

--- UnityGame/Assets/RotatorScript.cs
+++ UnityGame/Assets/RotatorScript.cs
@@ -13,6 +13,6 @@ public class RotatorScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.localRotation = Quaternion.Euler(transform.eulerAngles + new Vector3(0, 0, 1));
+        transform.localRotation = Quaternion.Euler(transform.eulerAngles + new Vector3(0.2f, 0.1f, 1));
     }
 }
