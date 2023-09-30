47bd8a1aa1e7baaef60d23e22accecbfa0568fb9
Frib <Frib>
2023-09-30 03:03:45 +0200

    Implemented signalR buttonstuff enzo

--- UnityGame/Assets/Scripts/SignalRTest.cs
+++ UnityGame/Assets/Scripts/SignalRTest.cs
@@ -5,54 +5,107 @@ using UnityEngine;
 
 public class SignalRTest : MonoBehaviour
 {
-    void Start()
+    public SignalR SignalR;
+    public MainMenu MainMenu;
+
+    private string DeveURL = "https://LD54_Server.Devedse.DuckDNS.org/UltraHub";
+
+    public void ReceiveRoomName(string name)
     {
-        // Initialize SignalR
-        var signalR = new SignalR();
-        signalR.Init("http://10.88.10.1:5281/UltraHub");
+        MainMenu.ShowHostScreen(name);
+    }
 
-        // Handler callbacks
-        signalR.On("Server_ReceiveButtonPress", (string payload) =>
-        {
-            // Deserialize payload A from JSON
-            var json = JsonUtility.FromJson<JsonPayload>(payload);
-            Debug.Log($"Server_ReceiveButtonPress: {payload}");
-        });
-        signalR.On("Server_ReceiveRoomName", (string payload) =>
-        {
-            // Deserialize payload B from JSON
-            //var json = JsonUtility.FromJson<JsonPayload>(payload);
-            //Debug.Log($"Server_ReceiveRoomName: {json.message}");
-            Debug.Log($"Server_ReceiveRoomName: {payload}");
-        });
+    public void OnHostLobby()
+    {
+        DontDestroyOnLoad(gameObject);// Connection callbacks
 
-        // Connection callbacks
-        signalR.ConnectionStarted += (object sender, ConnectionEventArgs e) =>
+        SetupSignalR();
+        SignalR.ConnectionStarted += (object sender, ConnectionEventArgs e) =>
         {
             // Log the connected ID
             Debug.Log($"Connected: {e.ConnectionId}");
 
             // Send payload A to hub as JSON
-            var json1 = new JsonPayload
-            {
-                message = "Hallo Devedse :)))"
-            };
-            signalR.Invoke("Server_CreateRoom", JsonUtility.ToJson(json1));
+            var message = "Hallo Devedse ik ben een scherm :)))";
+            SignalR.Invoke("Server_CreateRoom", message);
         };
-        signalR.ConnectionClosed += (object sender, ConnectionEventArgs e) =>
+        SignalR.Connect();
+    }
+
+    public void SetupSignalR()
+    {
+        SignalR = new SignalR();
+#if UNITY_EDITOR
+        //DeveURL = "http://10.88.10.1:5281/UltraHub";
+#endif
+        SignalR.Init(DeveURL);
+
+        // Handler callbacks
+        SignalR.On("Server_ReceiveButtonPress", (string payload) =>
+        {
+            // Deserialize payload A from JSON
+            Debug.Log($"Server_ReceiveButtonPress: {payload}");
+        });
+        SignalR.On("Server_ReceiveRoomName", (string payload) =>
+        {
+            // Deserialize payload B from JSON
+            //var json = JsonUtility.FromJson<JsonPayload>(payload);
+            //Debug.Log($"Server_ReceiveRoomName: {json.message}");
+            Debug.Log($"Server_ReceiveRoomName: {payload}");
+            ReceiveRoomName(payload);
+        });
+        SignalR.On("Server_ReceiveJoinRoom", (string payload) =>
+        {
+            // Deserialize payload B from JSON
+            //var json = JsonUtility.FromJson<JsonPayload>(payload);
+            //Debug.Log($"Server_ReceiveRoomName: {json.message}");
+            Debug.Log($"Server_ReceiveJoinRoom: {payload}");
+        });
+        SignalR.On("Client_JoinRoomResult", (bool payload) =>
+        {
+            // Deserialize payload B from JSON
+            //var json = JsonUtility.FromJson<JsonPayload>(payload);
+            //Debug.Log($"Server_ReceiveRoomName: {json.message}");
+            Debug.Log($"Client_JoinRoomResult: {payload}");
+            if (payload)
+            {
+                MainMenu.ShowButtonScreen();
+            }
+        });
+
+        SignalR.ConnectionClosed += (object sender, ConnectionEventArgs e) =>
         {
             // Log the disconnected ID
             Debug.Log($"Disconnected: {e.ConnectionId}");
         };
 
-        signalR.Connect();
-        Debug.Log("hallo");
     }
 
-    [Serializable]
-    public class JsonPayload
+    public void OnJoinLobby(string lobbyCode)
     {
-        public string message;
+        SetupSignalR();
+        SignalR.ConnectionStarted += (object sender, ConnectionEventArgs e) =>
+        {
+            // Log the connected ID
+            Debug.Log($"Connected: {e.ConnectionId}");
+
+            JoinRoom(lobbyCode, "nietdevedse");
+        };
+        SignalR.Connect();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    public void JoinRoom(string roomId, string clientName)
+    {
+        SignalR.Invoke("Client_JoinRoom", roomId, clientName);
+    }
+    public void SendButtonPress(int button, bool pressed)
+    {
+        SignalR.Invoke("Client_SendButtonPress", button, pressed);
     }
 
     // Update is called once per frame
---
+++ UnityGame/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject MainMenuPanel;
+    public GameObject HostPanel;
+    public GameObject ButtonPanel;
+    public TMP_InputField RoomInput;
+
+    public HostScreen HostScreen;
+    public SignalRTest SignalR;
+
+    internal void ShowHostScreen(string name)
+    {
+        HideAll();
+        HostPanel.SetActive(true);
+        HostScreen.RoomText.text = name;
+    }
+
+    private void HideAll()
+    {
+        MainMenuPanel.SetActive(false);
+        HostPanel.SetActive(false);
+        ButtonPanel.SetActive(false);
+    }
+
+    public void OnJoinClick()
+    {
+        string room = RoomInput.text;
+        SignalR.OnJoinLobby(room);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HideAll();
+        MainMenuPanel.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    internal void ShowButtonScreen()
+    {
+        HideAll();
+        ButtonPanel.SetActive(true);
+    }
+}
