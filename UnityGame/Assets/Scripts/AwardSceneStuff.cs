12f160cc3b6701881705548be323f3121581b126
Frib <Frib>
2023-10-01 04:47:58 +0200

    Added claim scene

--- UnityGame/Assets/Scripts/AwardSceneStuff.cs
+++ UnityGame/Assets/Scripts/AwardSceneStuff.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class AwardSceneStuff : MonoBehaviour
@@ -18,6 +19,12 @@ public class AwardSceneStuff : MonoBehaviour
     public GameObject RewardInstance;
 
     public int CurrentShipOwnerIndex;
+
+
+    public TextMeshProUGUI LeftButton;
+    public TextMeshProUGUI MidButton;
+    public TextMeshProUGUI RightButton;
+    public TextMeshProUGUI HelpText;
 
     // Start is called before the first frame update
     void Start()
@@ -40,6 +47,9 @@ public class AwardSceneStuff : MonoBehaviour
         Player.OnButton0Press.AddListener(() => SelectPlayer(true));
         Player.OnButton2Press.AddListener(() => SelectPlayer(false));
         Player.OnButton1Press.AddListener(() => ChooseCurrentShip());
+
+        MidButton.text = $"Add {RewardInstance.GetComponent<Module>().DisplayName} to the ship of {Player.PlayerName}";
+        HelpText.text = $"Congratulations, {Player.PlayerName}! You have won the right to place module {RewardInstance.GetComponent<Module>().DisplayName} on a ship of your choice! This module is {(MinigameManager.Instance.AllModules.Positives.Any(x => x.GetComponent<Module>().ModuleType == RewardInstance.GetComponent<Module>().ModuleType) ? "helpful" : "harmful")} to a ship!";
     }
 
     private void ChooseCurrentShip()
@@ -48,22 +58,96 @@ public class AwardSceneStuff : MonoBehaviour
         Player.OnButton0Press.AddListener(() => ApplyToSlot(0));
         Player.OnButton1Press.AddListener(() => ApplyToSlot(1));
         Player.OnButton2Press.AddListener(() => ApplyToSlot(2));
+
+        HelpText.text = $"Please choose a slot to place the {RewardInstance.GetComponent<Module>().DisplayName} on!";
+        var leftMod = ssf.FrontSocket.GetComponent<Module>();
+        var midMod = ssf.FrontSocket.GetComponent<Module>();
+        var rightMod = ssf.FrontSocket.GetComponent<Module>();
+
+        if (leftMod)
+            LeftButton.text = $"Replace {leftMod.DisplayName}";
+        else
+            LeftButton.text = $"Place on front";
+
+        if (midMod)
+            MidButton.text = $"Replace {midMod.DisplayName}";
+        else
+            MidButton.text = $"Place in middle";
+
+        if (rightMod)
+            RightButton.text = $"Replace {rightMod.DisplayName}";
+        else
+            RightButton.text = $"Place on rear";
     }
 
     public void ApplyToSlot(int slot)
     {
         var current = MinigameManager.Instance.SignalR.GetPlayerByNumber(CurrentShipOwnerIndex);
         current.SetModuleForSlot(slot, RewardPrefab.GetComponent<Module>().ModuleType);
+        Player.ResetButtonBindings();
+        StartCoroutine(SwapAndEnd(slot));
+    }
+
+    private IEnumerator SwapAndEnd(int slot)
+    {
+        var targetSocket = ssf.GetSocket(slot).transform;
+        var targetPos = targetSocket.position;
+        var socketStartPos = targetSocket.position;
+        var socketEndPos = socketStartPos + new Vector3(0, -4, 0);
+        var startPos = RewardInstance.transform.position;
+        var startRot = RewardInstance.transform.rotation;
+        float timer = 0;
+        while (timer < 1)
+        {
+            timer += Time.deltaTime;
+            RewardInstance.transform.position = Vector3.Lerp(startPos, targetPos, timer);
+            RewardInstance.transform.rotation = Quaternion.Lerp(startRot, targetSocket.rotation, timer);
+            targetSocket.transform.position = Vector3.Lerp(socketStartPos, socketEndPos, timer);
+            yield return null;
+        }
+
+        Destroy(ShipInstance);
+        Destroy(RewardInstance);
+        ShipInstance = Instantiate(ShipPrefab);
+        ShipInstance.transform.position = Target.position;
+        ShipInstance.transform.LookAt(-Spawn.position);
+        ssf = ShipInstance.GetComponent<SpaceShipFiller>();
+        ssf.SetProps(MinigameManager.Instance.SignalR.GetPlayerByNumber(CurrentShipOwnerIndex), CurrentShipOwnerIndex == Player.PlayerIndex ? FaceType.Happy : FaceType.Mad);
+
+        StartCoroutine(AnimateSeat());
+        yield return new WaitForSeconds(5);
+        MinigameManager.Instance.StartNextGame();
+    }
+
+    public IEnumerator AnimateSeat()
+    {
+        float timer = 0;
+        while (timer < 5)
+        {
+            timer += Time.deltaTime;
+
+            float bla = timer - (int)timer;
+            if (bla < .5f)
+                ssf.SetProps(MinigameManager.Instance.SignalR.GetPlayerByNumber(CurrentShipOwnerIndex), CurrentShipOwnerIndex == Player.PlayerIndex ? FaceType.Happy : FaceType.Mad);
+            else
+                ssf.SetProps(MinigameManager.Instance.SignalR.GetPlayerByNumber(CurrentShipOwnerIndex), FaceType.Normal);
+
+            yield return null;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        ShipInstance.transform.position = Vector3.Lerp(ShipInstance.transform.position, Target.position, .05f);
+        if (ShipInstance)
+        {
+            ShipInstance.transform.position = Vector3.Lerp(ShipInstance.transform.position, Target.position, .05f);
+        }
         if (OldShipInstance)
         {
             OldShipInstance.transform.position = Vector3.Lerp(OldShipInstance.transform.position, -Spawn.position, .02f);
         }
+
     }
 
     public void SelectPlayer(bool left)
@@ -86,6 +170,8 @@ public class AwardSceneStuff : MonoBehaviour
         ShipInstance.transform.LookAt(Target);
         ssf = ShipInstance.GetComponent<SpaceShipFiller>();
         ssf.SetProps(MinigameManager.Instance.SignalR.GetPlayerByNumber(CurrentShipOwnerIndex), CurrentShipOwnerIndex == Player.PlayerIndex ? FaceType.Happy : FaceType.Mad);
+
+        MidButton.text = $"Add {RewardInstance.GetComponent<Module>().DisplayName} to the ship of {MinigameManager.Instance.SignalR.GetPlayerByNumber(CurrentShipOwnerIndex).PlayerName}";
 
     }
 }
