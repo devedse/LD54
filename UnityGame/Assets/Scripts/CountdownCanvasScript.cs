94945f01f139ecd754b0a9084d2c3a1c6c1a1c8a
Frib <Frib>
2023-10-02 23:15:24 +0200

    asdf

--- UnityGame/Assets/Scripts/CountdownCanvasScript.cs
+++ UnityGame/Assets/Scripts/CountdownCanvasScript.cs
@@ -28,8 +28,8 @@ public class CountdownCanvasScript : MonoBehaviour
         Reward = rewardInstance.GetComponent<Module>();
         rewardInstance.SetActive(false);
 
-        //rewardInstance.AddComponent<RotatorScript>().RotatoSpeed = new Vector3(0, 90, 0);
-        //RewardParent.position = Camera.main.transform.position + (Camera.main.transform.forward * 1.5f) + (Camera.main.transform.up * -.4f);
+        rewardInstance.AddComponent<RotatorScript>().RotatoSpeed = new Vector3(0, 90, 0);
+        RewardParent.position = Camera.main.transform.position + (Camera.main.transform.forward * 1.5f) + (Camera.main.transform.up * -.4f);
         startTime = Time.time;
         gameObject.SetActive(true);
     }
